4a5f86b7781f7c49e2d91da9c20dfa1420d696a7
gh <cfhmgh@163.com>
2025-05-09 13:59:49 +0000

    Remove useless dependencies of jemalloc and rocksdb

--- tests/test_cpu.cpp
+++ tests/test_cpu.cpp
@@ -15,7 +15,7 @@ int has_avx512() {
   __cpuid_count(7, 0, info[0], info[1], info[2], info[3]);
 #endif
   int support_avx512 = (info[1] & (1 << 16)) != 0;  // AVX512F bit
-  int support_avx2 = (info[1] & (1 << 5)) != 0;  // AVX2 bit
+  int support_avx2 = (info[1] & (1 << 5)) != 0;     // AVX2 bit
   return support_avx512 << 1 | support_avx2;
 }
 
